26830404075e9c898b596c0fad327fa09566c35f
Harry-Gupenec <Spektar001@mail.ru>
2023-10-14 03:37:06 +0300

    feat: added styles to the header and footer files

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,11 +1,20 @@
-import styles from './Footer.module.scss';
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Footer
+      <p className={styles.footer__title}>
+        Created by &copy;
+        <a
+          className={styles.footer__link}
+          href="https://github.com/Spektar001"
+          target="_blank"
+        >
+          Harry Gupenec
+        </a>
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
